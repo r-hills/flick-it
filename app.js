8df83e5db0f8cffefac0ec39f2375a77636c433b
Rick Hills <rh-jobs@outlook.com>
2015-10-02 20:43:33 -0700

    Clean up CSS Stlying.

--- app.js
+++ app.js
@@ -4,19 +4,33 @@ flickit.config(function($stateProvider, $urlRouterProvider) {
 
     $stateProvider.state('welcome', {
         url: "",
-        templateUrl: "partials/welcome.html"
+        views: {
+            'welcome': {
+                templateUrl: "partials/welcome.html"
+            }
+        }
     });
 
     $stateProvider.state('flickit', {
         url: "/flickit",
-        templateUrl: "partials/flickit.html",
-        controller: "ReviewsCtrl"
+        views: {
+            'header': {
+                templateUrl: "partials/header.html"
+            },
+            'flickit': {
+                templateUrl: "partials/flickit.html",
+                controller: "ReviewsCtrl"            }
+        }
     });
 
     $stateProvider.state('flickit.main', {
         url: "/:reviewId",
-        templateUrl: "partials/flickit.main.html",
-        controller: 'ReviewsCtrl'
+        views: {
+            'flickit': {
+                templateUrl: "partials/flickit.main.html",
+                controller: 'ReviewsCtrl'
+            }
+        }
     });
 
 
--- services/ReviewsFactory.js
+++ services/ReviewsFactory.js
@@ -5,20 +5,24 @@ flickit.factory('ReviewsFactory', function ReviewsFactory() {
     //console.log(factory.reviews);
     factory.reviews = [ { id: 1,
                           title: "Enemy of The State was good",
-                          text:  "Although it was made a number of years ago, I liked it"
+                          text:  "Although it was made a number of years ago, I liked it. Bacon ipsum dolor amet alcatra pork shankle"
                         },
                         { id: 2,
-                          title: "Gone Girl",
-                          text:  "Pretty good. Bacon ipsum dolor amet alcatra pork shankle, fatback beef brisket picanha tenderloin. Pork chop spare ribs landjaeger pork loin strip steak, sirloin filet mignon bacon beef."
+                          title: "Gone Girl - a great psychological thriller",
+                          text:  "Pretty good. Capicola boudin flank pork loin frankfurter cupim beef ribs rump venison swine drumstick, fatback beef brisket picanha tenderloin. Pork chop spare ribs landjaeger pork loin strip steak, sirloin filet mignon bacon beef."
                         },
                         { id: 3,
                           title: "Fight Club, blah blan blah",
                           text:  "Pretty good. Turkey alcatra hamburger fatback salami meatball. Ball tip picanha tri-tip ham, shankle rump prosciutto venison."
                         },                        
                         { id: 4,
-                          title: "Flight with Denzel Washington",
+                          title: "\"Flight\" with Denzel Washington",
                           text:  "Tail sausage meatball drumstick ball tip kevin. Kielbasa pork loin short loin ribeye. Doner biltong cow spare ribs, ground round ball tip tongue alcatra meatloaf porchetta tail."
-                        }
+                        },
+                        { id: 5,
+                          title: "Boyhood - I'll never get those 4 hours of my life back",
+                          text:  "Baconipsum.com - Turkey alcatra hamburger fatback salami meatball. Ball tip picanha tri-tip ham, shankle rump prosciutto venison."
+                        }                        
                       ];
 
 
@@ -28,6 +32,8 @@ flickit.factory('ReviewsFactory', function ReviewsFactory() {
                                 title:    factory.reviewTitle,
                                 text:     factory.reviewText
                             });
+        factory.reviewTitle = null;
+        factory.reviewText  = null; 
     };
     ///console.log(factory.reviews);
     
